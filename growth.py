de072a3844a15122c9d4eb95a42a5206f5cb3621
Parison Corentin <c.parison@juggle.fr>
2019-11-26 15:25:28 +0100

    [Upadate] python script

--- growth.py
+++ growth.py
@@ -3,6 +3,9 @@ import sys
 import csv
 import string
 import time
+import os.path
+from os import path
+
 
 import random
 
@@ -14,9 +17,49 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
+import argparse
+import subprocess
+parser = argparse.ArgumentParser()
+parser.add_argument("--driver", "-d", help="choose your driver with value chrome or firefox")
+parser.add_argument("--install", "-i", help="Install driver chrome || firefox")
+#parser.add_argument("--search", "-s", help="Use -s || --search to make search without using parametre.py ")
+
+args = parser.parse_args()
+if args.install == "chrome":
+    subprocess.call(["bash","install/chrome.sh"])
+if args.install == "firefox":
+    subprocess.call(["bash","install/firefox.sh"])
+    
+if args.driver == "chrome":
+    print ("driver is chrome")
+    print ("Check if webdriver chrome exist")
+    if path.exists("./chromedriver"):
+        print "webdriver chrome exist, continue !\n"
+        driver = webdriver.Chrome('./chromedriver')
+    else:
+        print "Webdriver not present, please use -h for help ! "
+        sys.exit()
+       
+
+elif args.driver == "firefox":
+    print("driver is firefox")
+    if path.exists("/usr/local/bin/geckodriver") or path.exists("/usr/bin/geckodriver"):
+        print "Ok, geckodriver is present, continue..."
+        driver = webdriver.Firefox()
+    else:
+        print "Geckodriver is not present, please use growth -i firefox"
+        sys.exit()
+else:
+    print ("Expected one argument, chrome or firefox ! Use grotwh.py -d chrome or -d firefox")
+    sys.exit()
+
+#if args.driver == "search":
+    
+
 
 def pause():
     time_break = random.randint(1,2)
+    # print "Pause de " + str(time_break) + " seconde(s)."
     return time.sleep(time_break)
 
   
@@ -24,7 +67,6 @@ writer = csv.writer(open(parametre.file_name, 'wb'))
 
 writer.writerow(['Name', 'Job Title', 'Company', 'College', 'Location', 'URL'])
 
-driver = webdriver.Chrome('./chromedriver')
 
 driver.get('https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
 
@@ -39,12 +81,9 @@ password.send_keys(parametre.linkedin_password)
 sign_in_button = driver.find_element_by_xpath('//*[@type="submit"]')
 
 sign_in_button.click()
-
 sleep(0.2)
-
 driver.get('https:www.google.com')
 sleep(1)
-
 search_query = driver.find_element_by_name('q')
 search_query.send_keys(parametre.search_query)
 sleep(0.5)
@@ -63,12 +102,14 @@ while nb > 0:
       writer.writerow(["","","","","",url_link.encode('utf-8')])
 
   try:
+#    wait = WebDriverWait(driver, 2)
     next = driver.find_element_by_xpath("//*[@id='pnnext']")
+#    next = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='pnnext'")))
+#    print(next)
     next.click()
   except:
     print ("failed")
     nb = 2
-
 
 
 driver.quit()
