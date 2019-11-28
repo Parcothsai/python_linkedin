f41386c3eb87c058a6abd29aec48a73474120556
Parison Corentin <c.parison@juggle.fr>
2019-11-28 12:52:04 +0100

    [Update] Infinite loop on last page google chrome is solved, but close script when google captcha is present

--- growth.py
+++ growth.py
@@ -8,17 +8,17 @@ from os import path
 
 
 import random
-
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
 import argparse
 import subprocess
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--driver", "-d", help="choose your driver with value chrome or firefox")
 parser.add_argument("--install", "-i", help="Install driver chrome || firefox")
@@ -82,7 +82,7 @@ sign_in_button = driver.find_element_by_xpath('//*[@type="submit"]')
 
 sign_in_button.click()
 sleep(0.2)
-driver.get('https:www.google.com')
+driver.get('https://www.google.com')
 sleep(1)
 search_query = driver.find_element_by_name('q')
 search_query.send_keys(parametre.search_query)
@@ -90,12 +90,12 @@ sleep(0.5)
 search_query.send_keys(Keys.RETURN)
 sleep(1)
 
-nb = 1
-while nb > 0:
+nb = 3
+while nb > 2:
   i = ["0","1","2","3","4","5","6","7","8","9","10","11"]
   for path in i:
     linkedin_urls = driver.find_elements_by_xpath("//*[@id='rso']/div/div/div[{}]/div/div/div[1]/a".format(path))
-  
+
     for url in linkedin_urls:
       print(url.get_attribute("href"))
       url_link = url.get_attribute("href")
@@ -103,17 +103,18 @@ while nb > 0:
 
   try:
 #    wait = WebDriverWait(driver, 2)
-    next = driver.find_element_by_xpath("//*[@id='pnnext']")
-#    next = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='pnnext'")))
-#    print(next)
+#    next = driver.find_element_by_xpath("//*[@id='pnnext']")
+    next = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, "//*[@id='pnnext']"))
+    )
     next.click()
-  except:
-    print ("failed")
-    nb = 2
+   # next = driver.wait.until(presence_of_element_located())
+#    print(next)
 
+  except:
+    print ("Failed access to next page OR google captcha is present, exit Script")
+    nb = 1
 
 driver.quit()
-
-
 
 
